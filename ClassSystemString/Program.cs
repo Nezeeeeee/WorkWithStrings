1e90be31e8df28f5ee0a9370c20ca0900dd3bd1d
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-04 18:38:35 +0300

    changeone

--- ClassSystemString/Program.cs
+++ ClassSystemString/Program.cs
@@ -10,6 +10,11 @@ namespace ClassSystemString
     {
         static void Main(string[] args)
         {
+            string s1 = "Hello";
+            string s2 = null;
+
+            string s3 = new String('a', 10);
+            string s4 = new String(new char[] { '1', 'a', 'b', 'c' });
         }
     }
 }
