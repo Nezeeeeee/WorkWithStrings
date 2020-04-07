4a230b995da80cd08e8e30c14879eccef6a1417b
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-07 18:28:05 +0300

    added 2 topic

---
+++ ClassStringBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region StringBuilder
+            StringBuilder sb = new StringBuilder("Привет мир");
+            Console.WriteLine($"Длина строки: {sb.Length}");
+            Console.WriteLine($"Ёмкость строки: {sb.Capacity}");
+            sb.Replace("мир", "Зилибоба!");
+            Console.WriteLine(sb);
+            #endregion
+        }
+    }
+}
---
+++ RegularExpressionsLesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region RegularExpressions
+            string s = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+            Regex regex = new Regex(@"туп(\w*)");
+            MatchCollection matches = regex.Matches(s);
+            if(matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+            #endregion
+        }
+    }
+}
